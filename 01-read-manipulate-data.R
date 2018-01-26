509a535f73df588259aadf79f16ede1c9635f8b0
luehkenecology <luehken65@gmail.com>
2018-01-26 13:59:39 +0100

    link to data order

--- 01-read-manipulate-data.R
+++ 01-read-manipulate-data.R
@@ -8,11 +8,11 @@ setwd(PROJHOME)
 library(xlsx)
 
 #
-albopictus1 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 1)
-albopictus2 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 2)
-albopictus3 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 3)
-albopictus4 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 4)
-albopictus5 <- read.xlsx("Aedes albopictus Italien 2017.xlsx", 1)
-albopictus6 <- read.xlsx("Aedes albopictus Italien 2017.xlsx", 2)
+albopictus1 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 1)
+albopictus2 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 2)
+albopictus3 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 3)
+albopictus4 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 4)
+albopictus5 <- read.xlsx("data/Aedes albopictus Italien 2017.xlsx", 1)
+albopictus6 <- read.xlsx("data/Aedes albopictus Italien 2017.xlsx", 2)
 
 
