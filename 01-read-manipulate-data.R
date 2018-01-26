b610e1127270345a064287b0de52b06976a09a4f
luehkenecology <luehken65@gmail.com>
2018-01-26 14:03:25 +0100

    delete rows of the NAs by NAs in the names of experiments

--- 01-read-manipulate-data.R
+++ 01-read-manipulate-data.R
@@ -7,7 +7,7 @@ setwd(PROJHOME)
 # read project
 library(xlsx)
 
-#
+# read
 albopictus1 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 1)
 albopictus2 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 2)
 albopictus3 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 3)
@@ -15,4 +15,11 @@ albopictus4 <- read.xlsx("data/Aedes albopictus Deutschland.xlsx", 4)
 albopictus5 <- read.xlsx("data/Aedes albopictus Italien 2017.xlsx", 1)
 albopictus6 <- read.xlsx("data/Aedes albopictus Italien 2017.xlsx", 2)
 
+# delete rows of the NAs by NAs in the names of experiments
+albopictus1_1 <- albopictus1[ !is.na(albopictus1[,2]), ]
+albopictus2_1 <- albopictus2[ !is.na(albopictus2[,2]), ]
+albopictus3_1 <- albopictus3[ !is.na(albopictus3[,2]), ]
+albopictus4_1 <- albopictus4[ !is.na(albopictus4[,2]), ]
+albopictus5_1 <- albopictus5[ !is.na(albopictus5[,2]), ]
+albopictus6_1 <- albopictus6[ !is.na(albopictus6[,2]), ]
 
