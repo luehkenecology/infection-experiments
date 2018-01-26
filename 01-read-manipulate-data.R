5728316a6b6f2a075534613a955766b6299f2033
luehkenecology <luehken65@gmail.com>
2018-01-26 13:58:27 +0100

    read data

--- 01-read-manipulate-data.R
+++ 01-read-manipulate-data.R
@@ -7,4 +7,12 @@ setwd(PROJHOME)
 # read project
 library(xlsx)
 
+#
+albopictus1 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 1)
+albopictus2 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 2)
+albopictus3 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 3)
+albopictus4 <- read.xlsx("Aedes albopictus Deutschland.xlsx", 4)
+albopictus5 <- read.xlsx("Aedes albopictus Italien 2017.xlsx", 1)
+albopictus6 <- read.xlsx("Aedes albopictus Italien 2017.xlsx", 2)
+
 
