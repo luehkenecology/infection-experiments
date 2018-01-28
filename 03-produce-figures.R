03172b55fa59db65866f81652f48f90a48a842eb
luehkenecology <luehken65@gmail.com>
2018-01-28 19:40:01 +0100

    add axis labs

--- 03-produce-figures.R
+++ 03-produce-figures.R
@@ -13,5 +13,7 @@ data <- read.table("output/data_rates_sub.csv", header = T, sep = ";")
 #
 ggplot(data, aes(as.factor(temperature), as.numeric(transmission_rate))) + 
   geom_point() +
+  xlab("Temperature (°C)") + 
+  ylab("Transmission rate (%)") +
   facet_wrap(~origin+dpi) + 
   theme_bw()
