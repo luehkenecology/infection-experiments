f25f263bc5aa2a4b288762386191a9b44d2e0b5e
luehkenecology <luehken65@gmail.com>
2018-01-28 20:41:12 +0100

    transmission plot update

--- 03-produce-figures.R
+++ 03-produce-figures.R
@@ -17,3 +17,14 @@ ggplot(data, aes(as.factor(temperature), as.numeric(transmission_rate))) +
   ylab("Transmission rate (%)") +
   facet_wrap(~origin+dpi) + 
   theme_bw()
+
+png("figure/tranmission_rate2.png",width = 5, height=3, units = 'in', res = 600)
+ggplot(data, aes(as.factor(temperature), (as.numeric(transmission_rate)))) +
+  geom_bar(fill = "black", col = "black", stat = "identity") +
+  facet_wrap(~ origin+dpi)  +
+  geom_text(aes(label = paste(formatC(round(transmission_rate,2), 2, format = "f"), "%", sep = " "), y = 0.5+(as.numeric(transmission_rate))+5), size = 3, position =  position_dodge(width = 1))+
+  ylab("Transmission rate (%)") +
+  xlab(expression("Temperature ("*degree*C*")"))+
+  scale_y_continuous(limits = c(0,45), expand = c(0, 0)) +
+  theme_bw()
+dev.off()
